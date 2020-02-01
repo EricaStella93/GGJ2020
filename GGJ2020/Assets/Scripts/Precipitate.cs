7532cf59f9e2d6d098f8ea581349b677762997e3
Idkwnisu <stefano-palma@hotmail.it>
2020-02-01 09:37:55 +0100

    Panel precipitate

---
+++ GGJ2020/Assets/Scripts/Precipitate.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Precipitate : MonoBehaviour
+{
+    private bool active = false;
+    public float speed = 3.0f;
+    public float acc = 2.0f;
+
+    public float damp = 0.3f;
+
+    public float targetY = 255; //to change later?
+
+    private Image image;
+
+    public Sprite winSprite;
+    public Sprite loseSprite;
+
+    public bool red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                            
+        if(active)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+            speed += acc;
+            if(transform.position.y <= targetY && speed > 0)
+            {
+                speed = -1*damp*speed;
+            }
+
+        }
+    }
+
+    public void Activate()
+    {
+        active = true;
+        if(PointManager.Instance.redWinning() == red)
+        {
+            image.sprite = winSprite;
+        }
+        else
+        {
+            image.sprite = loseSprite;
+        }
+    }
+}
