c4a0fb23d5a67959fbdd1c0333f6f06f45c91255
Idkwnisu <stefano-palma@hotmail.it>
2020-02-02 10:12:07 +0100

    image switching video

---
+++ GGJ2020/Assets/Scripts/ImageSwitching.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwitching : MonoBehaviour
+{
+    private Image image;
+
+    public Sprite image1;
+    public Sprite image2;
+
+    private int currentSprite = 1;
+
+    public int frameSkip = 10;
+
+    private int currentFrame = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentFrame++;
+        if(currentFrame >= frameSkip)
+        {
+            currentFrame = 0;
+            if(currentSprite == 1)
+            {
+                currentSprite = 2;
+                image.sprite = image2;
+            }
+            else if(currentSprite == 2)
+            {
+                currentSprite = 1;
+                image.sprite = image1;
+            }
+        }
+    }
+}
